350b6e8952caaaa067d5ecb14f2dbbff30a4b24d
Enzo <54595511+esportellini@users.noreply.github.com>
2024-04-01 18:44:21 -0300

    test

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="felx flex-col">
+      <p className="text-3l font-bold text-indigo-500">
+      HELLO DISCORD
+    </p>
+    <Button variant="default"> Click Me</Button>
+    </div>
+    
+  );
+}
