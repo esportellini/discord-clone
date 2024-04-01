7be1344b391ad544e4ed5103797a346bb48238c9
Enzo <54595511+esportellini@users.noreply.github.com>
2024-04-01 19:07:37 -0300

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { Children } from "react";
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
     return ( 
-        <div>
+        <div className="bg-red-500 h-full">
             {children}
         </div>
      );
